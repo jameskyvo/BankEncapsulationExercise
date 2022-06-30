18e0e3f5a09f57fe3fac44abfdfbde572172b811
gamejunkiez <queenangrylobster@gmail.com>
2022-06-30 10:20:02 -0500

    completed exercise

---
+++ BankEncapsulation/BankEncapsulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankEncapsulation
+    {
+        private double balance = 0;
+        public double Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+            return balance;
+        }
+        public double GetBalance()
+        {
+            Console.WriteLine($"${balance}");
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userAccount = new BankEncapsulation();
+            Console.WriteLine("Hello! Welcome to your fictional bank account. Please enter an amount you would like to deposit.");
+            var depositAmount = int.Parse(Console.ReadLine());
+            userAccount.Deposit(depositAmount);
+            userAccount.GetBalance();
+        }
+    }
+}
